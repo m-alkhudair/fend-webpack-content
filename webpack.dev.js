79987c47d2b44cbeca89921dd902950bb3095525
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2020-11-09 07:52:40 +0300

    Finished-the-walk-through

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,35 +1,34 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
+    entry: './src/client/index.js',
     stats: 'verbose',
     module: {
         rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                }
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
+        new CleanWebpackPlugin ({
             dry: true,
-            // Write Logs to Console
             verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new BundleAnalyzerPlugin()
     ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,23 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
     mode: 'production',
+    entry: './src/client/index.js',
     module: {
         rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                }
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
         })
     ]
 }
